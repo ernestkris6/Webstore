c863f3041ef412abb4637584fdcd583e826e7049
ERNEST CHRIS <ernestchris6@gmail.com>
2024-01-30 20:56:24 +0100

    slider

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import hero from '../..'
+
+const Slider = () => {
+  return (
+    <div>
+        <img src={} alt='slider' />
+    </div>
+  )
+}
+
+export default Slider;
