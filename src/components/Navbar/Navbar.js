6cd9027cb813800d444b53f0e16f27cc367aeea6
ERNEST CHRIS <ernestchris6@gmail.com>
2024-01-30 19:51:02 +0100

    font size config

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,15 +2,19 @@ import logo from '../../assets/logo.png'
 
 const Navbar = () => {
     return ( 
-        <>
+<>
     <div className="bg-black w-full p-2">
-        <h3 className="text-white font-inter text-2xl font-bold text-center">Welcome all</h3>
+        <h3 className="text-white font-inter text-1xl font-bold text-center lmobile:text-2xl">Welcome ALL</h3>
     </div>
 
     <div className="flex justify-around items-center">
         <div>
             <img className="h-28 w-full" src={logo} alt="store" />
         </div>
+    </div>
+
+    <div className='bg-black w-full p-1'>
+        <h4 className='text-white text-center font-inter text-sm'>AFFORDABLE PRICES AT IT'S PEAK</h4>
     </div>
 </>
     
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,33 @@ module.exports = {
       "Inter": ['Inter','sans-serif'],
     },
     extend: {
+      spacing: {},
+      borderRadius: {
+        "17xl": "36px",
+      },
+
+      fontSize: {
+        xl: "20px",
+        base: "16px",
+        sm: "14px",
+        lg: "18px",
+        "5xl": "24px",
+        "29xl": "48px",
+        inherit: "inherit",
+      },
       
+      screens: {
+        Lxl: "1420px",
+        large: "1340px",
+        semiLarge: "1180px",
+        Lmd: "900px",
+        xlmobile: "560px",
+        lmobile: "500px",
+        mobile: "431px",
+        smobile: "300px",
+        ss: "370px",
+        xs: "0px",
+      },
     },
   },
   plugins: [],
