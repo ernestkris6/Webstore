fe6edbd6f0368a8590ae763f977a26e61a14bfba
ERNEST CHRIS <ernestchris6@gmail.com>
2024-01-30 14:30:48 +0100

    logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,18 @@
+import logo from '../assets/logo.png'
+
 const Navbar = () => {
     return ( 
-    <div className="bg-black w-full p-12">
-        <h1 className="text-white  text-3xl">Navbar</h1>
+        <>
+    <div className="bg-black w-full p-2">
+        <h3 className="text-white font-inter text-2xl font-bold text-center">Welcome all</h3>
     </div>
+
+    <div className="flex justify-around items-center">
+        <div>
+            <img className="h-28 w-full" src={logo} alt="store" />
+        </div>
+    </div>
+</>
     
     );
 }
