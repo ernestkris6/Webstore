1638506e35aff26a065490957a1c098f7730e163
ERNEST CHRIS <ernestchris6@gmail.com>
2024-01-30 21:03:07 +0100

    slider img

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import hero from '../..'
+import hero from '../../assets/shoe4.jpg'
 
 const Slider = () => {
   return (
     <div>
-        <img src={} alt='slider' />
+        <img className='h-[550px] w-full' src={hero} alt='slider' />
     </div>
   )
 }
