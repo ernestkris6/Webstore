47389be8583e6fd38325a64e79010cde1be73c4a
ERNEST CHRIS <ernestchris6@gmail.com>
2024-02-01 22:59:13 +0100

    redux

--- src/Features/SliderSlice.jsx
+++ src/Features/SliderSlice.jsx
@@ -1,19 +1,18 @@
 import { createSlice, configureStore } from '@reduxjs/toolkit';
 
+
 export const sliderSlice = createSlice({
-    name: 'slider',
-    initialState: {
-        value: 0,
-        length: 4,
+    name : "slider",
+    value : 0,
+    length : 4
+},
+reducers = {
+    nextSlider(state, actions){
+        
     },
-    reducers: {
-        nextSlider(state, actions) {
-            
-        },
-        prevSlider(state, actions) {},
-        dotSlider(state, actions) {}
-    }
+    prevSlider(state, actions){},
+    dotSlider(state, actions){}
 })
 
-export const {nextSlider, prevSlider, dotSlider} = createSlice.actions;
-export default sliderSlice.reducer;
+export const {nextSlider, prevSlider, dotSlider} = sliderSlice.actions;
+export default createSlice.reducers;
