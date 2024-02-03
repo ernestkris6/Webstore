14a8cd5b9ddb1b78febd36b0a3720d0c3a5270c0
ERNEST CHRIS <ernestchris6@gmail.com>
2024-02-03 11:34:50 +0100

    shoes

---
+++ src/assets/Data/Data.jsx
@@ -0,0 +1,39 @@
+import shoe1 from '../../assets/shoe1.jpg';
+import shoe2 from '../../assets/shoe2.jpg';
+import shoe3 from '../../assets/shoe3.jpg';
+import shoe3 from '../../assets/shoe4.jpg';
+
+
+
+
+ export const SliderData = [
+    {
+        "id" : 0,
+        "img" : shoe1,
+        "text" : "Lorem ipsum dolor...",
+
+    },
+
+    {
+        "id" : 1,
+        "img" : shoe2,
+        "text" : "Lorem ipsum dolor...",
+
+    },
+
+    {
+        "id" : 2,
+        "img" : shoe3,
+        "text" : "Lorem ipsum dolor...",
+
+    },
+
+    {
+        "id" : 3,
+        "img" : shoe4,
+        "text" : "Lorem ipsum dolor...",
+
+    },
+    
+   ]
+
