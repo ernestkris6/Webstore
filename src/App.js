130c06ad3b96a00c65cf41562ef6d9d425f0fd60
ERNEST CHRIS <ernestchris6@gmail.com>
2024-02-03 11:09:08 +0100

    proider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import { store } from "./Features/store";
 import Main from "./components/Main/Main";
 
 const App = () => {
 
     return ( 
-    <div store={store} >
+    <div >
       <Main />
     </div>
      );
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import hero from '../../assets/shoe4.jpg'
+import hero from '../../assets/shoe4.jpg';
+
+import { nextSlide, prevSlide, dotSlide } from '../../Features/Slices/SliderSlice';
+import { useSelector, useDispatch } from 'react-redux';
+
 
 const Slider = () => {
+
+  const slideIndex = useSelector((state) => state.slider.value)
+  console.log("slideIndex", slideIndex);
+
+  const dispatch = useDispatch()
   return (
     <div>
         <img className='h-[550px] w-full' src={hero} alt='slider' />
 
-        <button>Next</button> <br></br>
+        <button onClick={()=> dispatch(nextSlide(slideIndex + 1))}>Next</button> <br></br>
         <button>Previous</button>
     </div>
   )
