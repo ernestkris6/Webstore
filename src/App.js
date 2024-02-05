7c8c029180678bc07a41de6b1cc8507557c85d3b
ERNEST CHRIS <ernestchris6@gmail.com>
2024-02-05 19:24:36 +0100

    router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import Main from "./components/Main/Main";
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 const App = () => {
 
     return ( 
-    <div >
-      <Main />
+    <div>
+      <BrowserRouter>
+      <Routes>
+        
+      </Routes>
+            <Main />
+      </BrowserRouter>
+     
     </div>
      );
 }
