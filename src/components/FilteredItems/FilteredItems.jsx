93c56244850ad8d987973737dd104e25cea64001
ERNEST CHRIS <ernestchris6@gmail.com>
2024-02-05 10:54:26 +0100

    shirts

--- src/components/FilteredItems/FilteredItems.jsx
+++ src/components/FilteredItems/FilteredItems.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
+import clothes from '../../assets/clothes.jpg';
+
 const FilteredItems = () => {
   return (
-    <div>FilteredItems</div>
+    <div>
+        <div>
+            <h2 className='flex flex flex-row 
+            items-center justify-center mx-auto w-[500px]
+            font-inter font-bold p-2 text-lg rounded-full bg-green-300 '>
+                GET YOUR ITEMS AT 
+            VERY AFFORDABLE PRICES.</h2>
+        </div>
+
+        <div>
+            <img className='flex flex-row mt-6 justify-center mx-auto item-center h-[600px] w-[1000px]' src={clothes} alt='filtered img' />
+        </div>
+    </div>
   )
 }
 
