54ce52f30345894884788462429863b5ec66935d
ERNEST CHRIS <ernestchris6@gmail.com>
2024-02-07 22:45:27 +0100

    svg corrected

--- src/components/FilteredProducts/FilteredProducts.jsx
+++ src/components/FilteredProducts/FilteredProducts.jsx
@@ -5,8 +5,8 @@ import { useParams } from 'react-router-dom';
 const FilteredProducts = () => {
   const products = useSelector((state) => state.product.filteredProducts);
   console.log("products", products);
-  const params = useParams();
-  console.log("params", params);
+  const { type } = useParams();
+  console.log("params", type);
 
 
   return(
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -47,11 +47,11 @@ const Slider = () => {
           <svg xmlns="http://www.w3.org/2000/svg" 
           fill="none" 
           viewBox="0 0 24 24" 
-          stroke-width="1.5" 
+          strokeWidth="1.5" 
           stroke="currentColor" 
-          class="w-6 h-6">
-          <path stroke-linecap="round" 
-          stroke-linejoin="round" 
+          className="w-6 h-6">
+          <path strokeLinecap="round" 
+          strokeLinejoin="round" 
           d="m8.25 4.5 7.5 7.5-7.5 7.5" />
         </svg>
         </button>
@@ -59,11 +59,11 @@ const Slider = () => {
           <svg xmlns="http://www.w3.org/2000/svg" 
           fill="none" 
           viewBox="0 0 24 24" 
-          stroke-width="1.5" 
+          strokeWidth="1.5" 
           stroke="currentColor" 
-          class="w-6 h-6">
-  <       path stroke-linecap="round" 
-          stroke-linejoin="round" 
+          className="w-6 h-6">
+  <       path strokeLinecap="round" 
+          strokeLinejoin="round" 
           d="M15.75 19.5 8.25 12l7.5-7.5" />
         </svg>
         </button>
