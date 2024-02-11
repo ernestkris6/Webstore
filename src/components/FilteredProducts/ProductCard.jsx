b148d7a403964e1e5d6641e92b1f58592e887cde
ERNEST CHRIS <ernestchris6@gmail.com>
2024-02-11 14:47:46 +0100

    new changes

--- src/components/FilteredProducts/ProductCard.jsx
+++ src/components/FilteredProducts/ProductCard.jsx
@@ -26,10 +26,10 @@ const ProductCard = ({id, name, text, img, price, color}) => {
         </Typography>
       </CardBody>
       <CardFooter divider className="flex flex-center justify-between py-3">
-      <Typography variant='small'>${price}</Typography>
+      <Typography variant='small' className='font-bold text-lg'>${price}</Typography>
       <Typography variant='small' color='gray' className='flex gap-1'>
         {color?.map((color, index) => {
-            return <i className='fas fa map-marker-alt fa-sm mt-[3px] p-2 mr-3 rounded-full w-4' 
+            return <i className='fas fa map-marker-alt fa-sm mt-[3px] p-3 mr-3 rounded-full w-4' 
             key={index}
             style={{backgroundColor: color}}></i>
         })}
