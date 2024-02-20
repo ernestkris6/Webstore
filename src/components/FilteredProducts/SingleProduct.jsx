0f6a1d77ecc4ed6a296d4b0026463a47c7f799cb
ERNEST CHRIS <ernestchris6@gmail.com>
2024-02-20 21:23:17 +0100

    products

--- src/components/FilteredProducts/SingleProduct.jsx
+++ src/components/FilteredProducts/SingleProduct.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
 const SingleProduct = () => {
+
+  const product = useSelector((state) => state.product.SingleProduct)
+
+  const id = useParams();
+
+
   return (
-    <div>SingleProduct</div>
+    <div>
+      {product}
+      
+    </div>
   )
 }
 
