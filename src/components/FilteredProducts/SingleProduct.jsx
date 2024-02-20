46974b997cd95b6a69414576cc7c51720f4c65e9
ERNEST CHRIS <ernestchris6@gmail.com>
2024-02-20 21:47:34 +0100

    single product

--- src/components/FilteredProducts/SingleProduct.jsx
+++ src/components/FilteredProducts/SingleProduct.jsx
@@ -5,14 +5,21 @@ import { useParams } from 'react-router-dom';
 
 const SingleProduct = () => {
 
-  const product = useSelector((state) => state.product.SingleProduct)
-
-  const id = useParams();
+  const product = useSelector((state) => state.product.singleProduct)
+  console.log("singleproducts", product);
+  const {id} = useParams();
 
 
   return (
     <div>
-      {product}
+     {product.filter((product)=> product.id === id).map((product, index)=> {
+      return <div 
+      key={index}
+      className='flex justify-center items-center py-16'
+      >
+        <img src={product.img} alt={product.name} />
+      </div>
+     })}
       
     </div>
   )
