0884f41445936aede812c87d329d82ea1fa83ef9
ERNEST CHRIS <ernestchris6@gmail.com>
2024-02-24 23:59:50 +0100

    label

--- src/components/FilteredProducts/SingleProduct.jsx
+++ src/components/FilteredProducts/SingleProduct.jsx
@@ -33,9 +33,17 @@ const SingleProduct = () => {
             <p className='text-orange-700 text-xl font-inter font-bold tracking-normal leading-none pb-4'>15% OFF</p>
             <p>{product.text}</p>
             <div className='pb-4'>
-              <select>
+              <div>
+              <label
+                          htmlFor="size"
+                          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                        >
+                          Pick a size
+                        </label>
+              <select className='className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'>
                 <option></option>
               </select>
+              </div>
             </div>
           </div>
           </div>
