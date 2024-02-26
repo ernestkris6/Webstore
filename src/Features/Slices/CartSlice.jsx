2d756cdee18c86638a576143ac009066b5dcab27
ERNEST CHRIS <ernestchris6@gmail.com>
2024-02-26 23:04:09 +0100

    cart slice reducer

---
+++ src/Features/Slices/CartSlice.jsx
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const cartSlice = createSlice({
+    name : "cart",
+    initialState: {
+        cart : [],
+        amount : 0,
+        totalAmount : 0,
+        totalPrice : 0,
+    },
+    reducers : {
+        addToCart(state, action){
+
+        }
+    }
+})
+
+export const { addToCart } = cartSlice.actions;
+export default cartSlice.reducer;
--- src/Features/store.js
+++ src/Features/store.js
@@ -1,13 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import sliderReducer from "../Features/Slices/SliderSlice";
-import productReducer from "../Features/Slices/ProductSlice"
+import productReducer from "../Features/Slices/ProductSlice";
+import cartReducer from "../Features/Slices/CartSlice";
+
+
 
 export const store = configureStore({
     reducer : {
         "slider" : sliderReducer,
         "product" : productReducer,
+        "cart" : cartReducer,
     },
 
-   
 });
