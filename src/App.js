9336eaea7506cc636843557d977a27b71db4f69d
ERNEST CHRIS <ernestchris6@gmail.com>
2024-02-26 23:55:19 +0100

    dispatch

--- src/App.js
+++ src/App.js
@@ -3,8 +3,18 @@ import FilteredProducts from "./components/FilteredProducts/FilteredProducts";
 import SingleProduct from "./components/FilteredProducts/SingleProduct";
 import Main from "./components/Main/Main";
 
+import { useSelector } from "react-redux";
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 const App = () => {
+
+  const cart = useSelector((state) => state.cart.cart)
+  const totalAmount = useSelector((state) => state.cart.totalAmount)
+  const totalPrice = useSelector((state) => state.cart.totalPrice)
+
+  console.log("cart", cart);
+  console.log("amount", totalAmount);
+  console.log("price", totalPrice);
 
     return ( 
     <div>
