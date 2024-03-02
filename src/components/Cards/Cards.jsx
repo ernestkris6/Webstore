51c2b68aad72d567e27a364fa4af9589a3e9d5c8
ERNEST CHRIS <ernestchris6@gmail.com>
2024-03-02 19:17:35 +0100

    cards mobile

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,41 +1,95 @@
 import React from 'react';
 
-import jamb from '../../assets/Ellipse 363.png';
-import cambridge from '../../assets/Ellipse 363 (1).png';
-import waec from '../../assets/Ellipse 363 (2).png';
-import gce from '../../assets/Ellipse 363 (3).png'
+import jamb from '../../assets/waec2.png';
+import cambridge from '../../assets/Ellipse 363 (4).png';
+import waec from '../../assets/Ellipse 363 (5).png';
+import gce from '../../assets/Ellipse 363 (6).png'
+import ui from '../../assets/Ellipse 363 (7).png';
+import unilag from '../../assets/unilag.png';
+import waec2 from '../../assets/waec2.png';
+import search from '../../assets/tabler_search.png';
+import filter from '../../assets/bi_filter.png';
+import icon1 from '../../assets/Frame 35224.png';
+
 
 const Cards = () => {
+  const data = {
+    text1 : "UTME",
+    text2 : "WASSCE",
+    text3 : "GCE Nov/Dec",
+    text4 : "CAMBRIDGE",
+    text5 : "UI P.UTME",
+    text6 : "UNILAG P.UTME"
+  }
+
+  const images = {
+    img1 : jamb,
+    img2 : waec,
+    img3 : gce,
+    img4 : cambridge,
+    img5 : ui,
+    img6 : unilag,
+    img7 : waec2,
+  }
+
+
+
   return (
     <section>
-      <div>
-        <h1 className='flex justify-center items-center'>Our Programs</h1>
-        <p className='flex justify-center items-center'>Select the exam you are preparing for</p>
+      <div className='flex flex-col justify-center items-center mt-24'>
+        <h1 className='text-3xl font-inter mb-3'>Our Programs</h1>
+        <p className='text-lg'>Select the exam you are preparing for</p>
+      </div>
+
+      <div className='flex ml-6 mt-12'>
+        <input className='p-3 border-rounded border-2 rounded-2xl w-full outline-none' type='text' placeholder='Search'/>
+       <img className='relative left-[-40px] h-6 mt-4' src={search} alt='search icon'/>
+        <div className='mt-3 p-0'>
+          <img className='mr-14' src={filter} alt='filter'/>
+        </div>
+      </div>
+
+      <div className='grid grid-cols-3 gap-6 px-6 mt-12'>
+        <div>
+            <img className='border-2 rounded-xl px-4 py-8' src={images.img5} alt='img'/>
+            <p className='text-center'>{data.text2}</p>
+        </div>
+        <div>
+           <img className='border-2 rounded-xl px-4 py-8' src={images.img2} alt='img'/>
+           <p className='text-center'>{data.text4}</p>
+        </div>
+        <div>
+        <img className='border-2 rounded-xl px-4 py-8' src={images.img3} alt='img'/>
+        <p  className='text-center'>{data.text3}</p>
+        </div>
+        <div>
+        <img className='border-2 rounded-xl px-4 py-8' src={images.img4} alt='img'/>
+        <p  className='text-center'>{data.text1}</p>
+        </div>
+        <div>
+        <img className='border-2 rounded-xl px-4 py-8' src={images.img5} alt='img'/>
+        <p  className='text-center'>{data.text2}</p>
+        </div>
+        <div>
+        <img className='border-2 rounded-xl px-6 py-10' src={images.img6} alt='img'/>
+        <p className='text-center'>{data.text2}</p>
+        </div>
+      </div>
+
+      <div className='flex flex-row gap-4 mt-8 justify-center'>
+        <div>
+            <button className='bg-white rounded-full border-2 transform rotate-180'>
+              <img src={icon1} alt='icon2'/>
+            </button>
+        </div>
+        <div>
+        <button className='bg-white rounded-full border-2'>
+              <img src={icon1} alt='icon1'/>
+            </button>
+        </div>
       </div>
     </section>
   )
 }
 
 export default Cards;
-
-
-{/* <div className='ml-[68px] mt-[80px] justify-center item-center lmobile:flex lmobile:flex-row lmobile:gap-28'>
-            
-            <div>
-            <img className='border-2 py-16 px-12 rounded-xl' src={jamb} alt='jamb logo' />
-            <h3 className='flex text-center justify-center text-xl font-inter font-bold mt-3'>UTME 2024 Prep </h3>
-            </div>
-            <div>
-            <img className='border-2 py-16 px-12 rounded-xl' src={cambridge} alt='cambridge logo' />
-            <h3 className='flex text-center justify-center text-xl font-inter font-bold mt-3'>CAMBRIDGE </h3>
-            </div>
-            <div>
-            <img className='border-2 py-16 px-12 rounded-xl' src={waec} alt='waec logo' />
-            <h3 className='flex text-center justify-center text-xl font-inter font-bold mt-3'>WASSCE</h3>
-            </div>
-            <div>
-            <img className='border-2 py-16 px-12 rounded-xl' src={gce} alt='jamb logo' />
-            <h3 className='flex text-center justify-center text-xl font-inter font-bold mt-3'>GCE Nov/Dec </h3>
-            </div>
-            
-    </div> */}
