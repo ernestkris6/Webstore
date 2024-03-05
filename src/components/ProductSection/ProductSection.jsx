eb1c2e89a795f4167665e6ee7a0f5bc71d4d09ae
ERNEST CHRIS <ernestchris6@gmail.com>
2024-03-05 18:06:43 +0100

    grid section

--- src/components/ProductSection/ProductSection.jsx
+++ src/components/ProductSection/ProductSection.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { storeData } from '../../assets/Data/Data';
+import ProductSectionItem from './ProductSectionItem';
 
 const ProductSection = () => {
+
   return (
-    <div>OtherItems</div>
+    <div>
+      <div className='bg-black mt-8 p-2 w-[50%] mx-auto rounded-md'>
+          <h2 className='text-red-600 text-center text-lg font-inter font-bold tracking-normal leading-none'>SUMMER SALE T-SHIRTS 30%</h2>
+      </div>
+      <div className='grid grid-cols-3 justify-items-center py-3 gap-4 mx-auto max-w-7xl'>
+        {storeData.slice(0,6).map((product, index) => {
+            return <div key={index}> 
+                <ProductSectionItem
+                id={product.id}
+                name={product.name}
+                img={product.img}
+                text={product.text}
+                price={product.price}
+                totalPrice={product.totalPrice}
+                color={product.color}
+                size={product.size} />
+            </div>
+        })}
+      </div>
+    </div>
   )
 }
 
--- src/components/ProductSection/ProductSectionItem.jsx
+++ src/components/ProductSection/ProductSectionItem.jsx
@@ -1,8 +1,77 @@
-import React from 'react'
+import React from 'react';
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Typography,
+    Tooltip,
+  } from "@material-tailwind/react";
+  import { Button } from '@material-tailwind/react';
 
-const ProductSectionItem = () => {
+  import { useDispatch } from 'react-redux';
+  import { addToCart } from '../../Features/Slices/CartSlice';
+  
+
+const ProductSectionItem = ({
+    id,
+    img,
+    name,
+    text,
+    size,
+    price,
+    color,
+    totalPrice
+
+}) => {
+
+    const dispatch =  useDispatch();
+
+    const defaultSize = size[0];
+    const defaultColor = color[0];
+
   return (
-    <div>ProductSectionItem</div>
+    <div>
+        <Card className="mt-6 w-96">
+      <CardHeader color="blue-gray" className="relative h-56">
+        <img
+          src={img} alt={name}
+        />
+      </CardHeader>
+      <CardBody className='text-center'>
+        <Typography variant="h5" color="blue-gray" className="mb-2">
+        {name}
+        </Typography>
+        <Typography color='gray' className='font-medium'>
+         {text}
+        </Typography>
+        <div className='flex justify-between items-center pt-4'>
+        <Typography>
+         Size left: {defaultSize}
+        </Typography>
+        <Typography color='gray' className='font-medium'>
+         Color: <span className='px-2 rounded-full ml-2' style={{backgroundColor:  {defaultColor} }}></span>
+        </Typography>
+        </div>
+      </CardBody>
+      <CardFooter className="flex justify-center gap-7 pt-2">
+        <Tooltip content='Add to Cart' placement='bottom'>
+            <Button onClick={()=> dispatch(addToCart({
+                id : id,
+                img : img,
+                text : text,
+                amount : 1,
+                price : price,
+                totalPrice : totalPrice,
+                size : defaultSize,
+                color : defaultColor,
+            }))} 
+                size='lg' color='gray' variant='outlined' 
+                ripple='true'>Add to Cart</Button>
+        </Tooltip>
+      </CardFooter>
+    </Card>
+    </div>
   )
 }
 
