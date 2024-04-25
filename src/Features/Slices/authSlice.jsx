ee50b19756fd75cf8b6aa8c1238bf71235b407bf
ERNEST CHRIS <ernestchris6@gmail.com>
2024-04-25 12:02:16 +0100

    save state

--- src/Features/Slices/authSlice.jsx
+++ src/Features/Slices/authSlice.jsx
@@ -13,7 +13,20 @@ export const authSlice = createSlice ({
 
     reducers: {
         login(state, action) {
-
+            userId = action.payload;
+            const userValidation =  /^[A-Za-z]{4,10}$/i.test(userId.name);
+            const passwordValidation = 
+            /^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{4,10}$/i
+            .test(userId.password);
+            state.user = userId;
+            if(!userValidation || !passwordValidation) {
+                state.user.authUser = false;
+            }
+            else{
+                state.user.authUser = true;
+                const saveState = JSON.stringify(userId);
+                sessionStorage.setItem("authUser", saveState);
+            }
         },
         logout(state, action){
 
