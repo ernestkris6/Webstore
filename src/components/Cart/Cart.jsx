b1ee862b39f954e252e56cc2ea4704ad94fb7d07
ERNEST CHRIS <ernestchris6@gmail.com>
2024-05-05 23:39:46 +0100

    cart styling

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,3 +1,144 @@
+// import { Fragment } from "react";
+// import {
+//   Button,
+//   Dialog,
+//   DialogHeader,
+//   DialogBody,
+//   DialogFooter,
+// } from "@material-tailwind/react";
+// import { Tooltip } from "@material-tailwind/react";
+// import { useSelector, useDispatch } from "react-redux";
+// import { removeFromCart } from "../../Features/Slices/CartSlice";
+
+// const Cart = ({ openModal, setOpen}) => {
+//   const cart = useSelector((state) => state.cart.cart);
+//   const totalPrice = useSelector((state) => state.cart.totalPrice);
+//   // const removeFromCart = useSelector((state) => state.cart.removeFromCart)
+//   const dispatch = useDispatch();
+  
+//   return (
+//     <div>
+//       {cart.length > 0 ? 
+//       <Fragment className="w-1/4 top-0 left-80 mx-auto right-8 border-0 outline-0"
+//       >
+//           <Dialog
+//             className="w-1/4 top-0 left-80 mx-auto right-8 border-0 outline-0"
+//             open={openModal}
+//             handler={() => setOpen(false)}
+//             animate={{
+//               mount: { scale: 1, y: 0 },
+//               unmount: { scale: 0.9, y: -100 },
+//             }}
+//           >
+//             <DialogHeader>Shopping Bag</DialogHeader>
+//             <DialogBody
+//               divider
+//               className="flex flex-col justify-center items-start"
+//             >
+//               {cart.map((item, index) => {
+//                 return (
+//                   <div key={index}>
+//                     <div className="grid grid-cols-2 py-4">
+//                       <div>
+//                         <img
+//                           className="h-[125px] rounded-md"
+//                           src={item.img}
+//                           alt={item.name}
+//                         ></img>
+//                         <div className="flex flex-col items-start">
+//                           <h4 className="text-black text-base font-inter font-bold tracking-normal leading-none pt-2">
+//                             {item.name}
+//                           </h4>
+//                         </div>
+//                         <div className="max-w-xs">
+//                           <p className="text-black text-xs font-inter tracking-normal leading-none pt-2">
+//                             {item.text}
+//                           </p>
+//                         </div>
+//                       </div>
+//                       <div>
+//                         <p className="text-black text-sm font-inter tracking-normal leading-none pt-2">
+//                           Selected size:{" "}
+//                           <span className="ml-2">{item.size}</span>
+//                         </p>
+//                         <p className="text-black text-sm font-inter tracking-normal leading-none pt-2">
+//                           Selected color:{" "}
+//                           <span
+//                             className="ml-2 rounded-full px-2"
+//                             style={{ backgroundColor: item.color }}
+//                           ></span>
+//                         </p>
+//                         <p className="text-black text-sm font-inter tracking-normal leading-none pt-2">
+//                           Amount: <span className="ml-2">{item.amount}</span>
+//                         </p>
+//                         <p className="text-black text-sm font-inter tracking-normal leading-none pt-2">
+//                           Single Item Price:{" "}
+//                           <span className="ml-2">${item.price}</span>
+//                         </p>
+//                         <p className="text-black text-sm font-inter tracking-normal leading-none pt-2">
+//                           Total Item Prices:{" "}
+//                           <span className="ml-2">${item.totalPrice}</span>
+//                         </p>
+//                         <div className="pt-4">
+//                           <Tooltip
+//                             content="Remove from the Cart"
+//                             placement="bottom"
+//                           >
+//                             <Button
+//                               onClick={() => dispatch(removeFromCart(item))}
+//                               size="sm"
+//                               color="red"
+//                               ripple={true}
+//                               variant="filled"
+//                               className="bg-red p-4"
+//                             >
+//                               Remove
+//                             </Button>
+//                           </Tooltip>
+//                         </div>
+//                       </div>
+//                     </div>
+//                   </div>
+//                 );
+//               })}
+//             </DialogBody>
+//             <DialogFooter className="flex justify-start items-center">
+//               <p className="text-black text-base font-inter tracking-normal leading-none pt-2">
+//                 Total Price of All Products:{" "}
+//                 <span className="ml-2">${totalPrice}</span>
+//               </p>
+//             </DialogFooter>
+//           </Dialog>
+//         </Fragment> : 
+//         <Fragment>
+//           <Dialog
+//             className="w-1/2 top-80 left-80 mx-8 right-80 border-0 outline-0"
+//             open={openModal}
+//             handler={() => setOpen(false)}
+//             animate={{
+//               mount: { scale: 1, y: 0 },
+//               unmount: { scale: 0.9, y: -100 },
+//             }}
+//           >
+//             <DialogHeader>Shopping Bag</DialogHeader>
+//             <DialogBody
+//               divider
+//               className="flex flex-col justify-center items-start"
+//             >
+//               <div>
+//                 <h1 className="text-black text-3xl font-inter font-bold tracking-normal leading-none py-4">Your bag is empty</h1>
+//                 <p className="text-black text-base font-inter tracking-normal leading-none py-4">Kindly add some products</p>
+//               </div>
+//             </DialogBody>
+//           </Dialog>
+//         </Fragment>}
+//     </div>
+//   )
+// }
+
+// export default Cart;
+
+
 import { Fragment } from "react";
 import {
   Button,
@@ -8,20 +149,20 @@ import {
 } from "@material-tailwind/react";
 import { Tooltip } from "@material-tailwind/react";
 import { useSelector, useDispatch } from "react-redux";
-import { removeFromCart } from "../../Features/Slices/CartSlice";
+import { removeFromCart } from  "../../Features/Slices/CartSlice";
 
-const Cart = ({ openModal, setOpen}) => {
+
+const Cart = ({ openModal, setOpen }) => {
   const cart = useSelector((state) => state.cart.cart);
   const totalPrice = useSelector((state) => state.cart.totalPrice);
-  // const removeFromCart = useSelector((state) => state.cart.removeFromCart)
+
   const dispatch = useDispatch();
-  
   return (
     <div>
-      {cart.length > 0 ? 
-      <Fragment>
+      {cart.length > 0 ? (
+        <Fragment>
           <Dialog
-            className="w-1/2 top-0 left-80 mx-8 right-80 border-0 outline-0"
+            className="w-[1/2] h-[100px] border-0 outline-10"
             open={openModal}
             handler={() => setOpen(false)}
             animate={{
@@ -32,7 +173,7 @@ const Cart = ({ openModal, setOpen}) => {
             <DialogHeader>Shopping Bag</DialogHeader>
             <DialogBody
               divider
-              className="flex flex-col justify-center items-start"
+              className="flex flex-col my-10 justify-center items-start"
             >
               {cart.map((item, index) => {
                 return (
@@ -72,11 +213,11 @@ const Cart = ({ openModal, setOpen}) => {
                         </p>
                         <p className="text-black text-sm font-inter tracking-normal leading-none pt-2">
                           Single Item Price:{" "}
-                          <span className="ml-2">${item.price}</span>
+                          <span className="ml-2">{item.price}$</span>
                         </p>
                         <p className="text-black text-sm font-inter tracking-normal leading-none pt-2">
                           Total Item Prices:{" "}
-                          <span className="ml-2">${item.totalPrice}</span>
+                          <span className="ml-2">{item.totalPrice}$</span>
                         </p>
                         <div className="pt-4">
                           <Tooltip
@@ -89,7 +230,6 @@ const Cart = ({ openModal, setOpen}) => {
                               color="red"
                               ripple={true}
                               variant="filled"
-                              className="bg-red p-4"
                             >
                               Remove
                             </Button>
@@ -104,14 +244,15 @@ const Cart = ({ openModal, setOpen}) => {
             <DialogFooter className="flex justify-start items-center">
               <p className="text-black text-base font-inter tracking-normal leading-none pt-2">
                 Total Price of All Products:{" "}
-                <span className="ml-2">${totalPrice}</span>
+                <span className="ml-2">{totalPrice}$</span>
               </p>
             </DialogFooter>
           </Dialog>
-        </Fragment> : 
+        </Fragment>
+      ) : (
         <Fragment>
           <Dialog
-            className="w-1/2 top-80 left-80 mx-8 right-80 border-0 outline-0"
+            className="border-0 outline-0"
             open={openModal}
             handler={() => setOpen(false)}
             animate={{
@@ -120,19 +261,21 @@ const Cart = ({ openModal, setOpen}) => {
             }}
           >
             <DialogHeader>Shopping Bag</DialogHeader>
-            <DialogBody
-              divider
-              className="flex flex-col justify-center items-start"
-            >
+            <DialogBody divider>
               <div>
-                <h1 className="text-black text-3xl font-inter font-bold tracking-normal leading-none py-4">Your bag is empty</h1>
-                <p className="text-black text-base font-inter tracking-normal leading-none py-4">Kindly add some products</p>
+                <h1 className="text-black text-3xl font-inter font-bold tracking-normal leading-none py-4">
+                  Your bag is empty
+                </h1>
+                <p className="text-black text-base font-inter tracking-normal leading-none ">
+                  Add some products
+                </p>
               </div>
             </DialogBody>
           </Dialog>
-        </Fragment>}
+        </Fragment>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Cart;
