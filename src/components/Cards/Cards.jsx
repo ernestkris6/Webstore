9f0272891d7e70aca3868495981b1bccdabacba9
ERNEST CHRIS <ernestchris6@gmail.com>
2024-05-07 21:56:35 +0100

    redesigning cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -13,83 +13,139 @@ import icon1 from '../../assets/Frame 35224.png';
 
 
 const Cards = () => {
-  const data = {
-    text1 : "UTME",
-    text2 : "WASSCE",
-    text3 : "GCE Nov/Dec",
-    text4 : "CAMBRIDGE",
-    text5 : "UI P.UTME",
-    text6 : "UNILAG P.UTME"
-  }
+  return(
+    <div>
 
-  const images = {
-    img1 : jamb,
-    img2 : waec,
-    img3 : gce,
-    img4 : cambridge,
-    img5 : ui,
-    img6 : unilag,
-    img7 : waec2,
-  }
-
-
-
-  return (
-    <section>
-      <div className='flex flex-col justify-center items-center mt-24'>
-        <h1 className='text-3xl font-inter mb-3'>Our Programs</h1>
-        <p className='text-lg'>Select the exam you are preparing for</p>
-      </div>
-
-      <div className='flex ml-6 mt-12'>
-        <input className='p-3 border-rounded border-2 rounded-2xl w-full outline-none lmobile:w-[480px] lmobile:py-1 lmobile:ml-12' type='text' placeholder='Search'/>
-       <img className='relative left-[-40px] h-6 mt-4' src={search} alt='search icon'/>
-        <div className='mt-3 p-0'>
-          <img className='mr-14 lmobile:border-2 rounded-2xl lmobile:px-2 lmobile:py-1 lmobile:mb-3' src={filter} alt='filter'/>
-        </div>
-      </div>
-
-      <div className='grid grid-cols-3 gap-6 px-6 mt-12 lmobile:grid-cols-4 lmobile:m-12'>
-        <div>
-            <img className='border-2 rounded-xl px-4 py-8' src={images.img5} alt='img'/>
-            <p className='text-center'>{data.text2}</p>
-        </div>
-        <div>
-           <img className='border-2 rounded-xl px-4 py-8' src={images.img2} alt='img'/>
-           <p className='text-center'>{data.text4}</p>
-        </div>
-        <div>
-        <img className='border-2 rounded-xl px-4 py-8' src={images.img3} alt='img'/>
-        <p  className='text-center'>{data.text3}</p>
-        </div>
-        <div>
-        <img className='border-2 rounded-xl px-4 py-8' src={images.img4} alt='img'/>
-        <p  className='text-center'>{data.text1}</p>
-        </div>
-        <div>
-        <img className='border-2 rounded-xl px-4 py-8' src={images.img5} alt='img'/>
-        <p  className='text-center'>{data.text2}</p>
-        </div>
-        <div>
-        <img className='border-2 rounded-xl px-6 py-10' src={images.img2} alt='img'/>
-        <p className='text-center'>{data.text2}</p>
-        </div>
-      </div>
-
-      <div className='flex flex-row gap-4 mt-8 justify-center lmobile:top-20'>
-        <div>
-            <button className='bg-white rounded-full border-2 transform rotate-180'>
-              <img src={icon1} alt='icon2'/>
-            </button>
-        </div>
-        <div>
-        <button className='bg-white rounded-full border-2'>
-              <img src={icon1} alt='icon1'/>
-            </button>
-        </div>
-      </div>
-    </section>
+    </div>
   )
 }
 
+
 export default Cards;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const Cards = () => {
+  // const data = {
+  //   text1 : "UTME",
+  //   text2 : "WASSCE",
+  //   text3 : "GCE Nov/Dec",
+  //   text4 : "CAMBRIDGE",
+  //   text5 : "UI P.UTME",
+  //   text6 : "UNILAG P.UTME"
+  // }
+
+  // const images = {
+  //   img1 : jamb,
+  //   img2 : waec,
+  //   img3 : gce,
+  //   img4 : cambridge,
+  //   img5 : ui,
+  //   img6 : unilag,
+  //   img7 : waec2,
+  // }
+
+
+
+  //return (
+  //   <section>
+  //     <div className='flex flex-col justify-center items-center mt-24'>
+  //       <h1 className='text-3xl font-inter mb-3'>Our Programs</h1>
+  //       <p className='text-lg'>Select the exam you are preparing for</p>
+  //     </div>
+
+  //     <div className='flex ml-6 mt-12'>
+  //       <input className='p-3 border-rounded border-2 rounded-2xl w-full outline-none lmobile:w-[480px] lmobile:py-1 lmobile:ml-12' type='text' placeholder='Search'/>
+  //      <img className='relative left-[-40px] h-6 mt-4' src={search} alt='search icon'/>
+  //       <div className='mt-3 p-0'>
+  //         <img className='mr-14 lmobile:border-2 rounded-2xl lmobile:px-2 lmobile:py-1 lmobile:mb-3' src={filter} alt='filter'/>
+  //       </div>
+  //     </div>
+
+  //     <div className='grid grid-cols-3 gap-6 px-6 mt-12 lmobile:grid-cols-4 lmobile:m-12'>
+  //       <div>
+  //           <img className='border-2 rounded-xl px-4 py-8' src={images.img5} alt='img'/>
+  //           <p className='text-center'>{data.text2}</p>
+  //       </div>
+  //       <div>
+  //          <img className='border-2 rounded-xl px-4 py-8' src={images.img2} alt='img'/>
+  //          <p className='text-center'>{data.text4}</p>
+  //       </div>
+  //       <div>
+  //       <img className='border-2 rounded-xl px-4 py-8' src={images.img3} alt='img'/>
+  //       <p  className='text-center'>{data.text3}</p>
+  //       </div>
+  //       <div>
+  //       <img className='border-2 rounded-xl px-4 py-8' src={images.img4} alt='img'/>
+  //       <p  className='text-center'>{data.text1}</p>
+  //       </div>
+  //       <div>
+  //       <img className='border-2 rounded-xl px-4 py-8' src={images.img5} alt='img'/>
+  //       <p  className='text-center'>{data.text2}</p>
+  //       </div>
+  //       <div>
+  //       <img className='border-2 rounded-xl px-6 py-10' src={images.img2} alt='img'/>
+  //       <p className='text-center'>{data.text2}</p>
+  //       </div>
+  //     </div>
+
+  //     <div className='flex flex-row gap-4 mt-8 justify-center lmobile:top-20'>
+  //       <div>
+  //           <button className='bg-white rounded-full border-2 transform rotate-180'>
+  //             <img src={icon1} alt='icon2'/>
+  //           </button>
+  //       </div>
+  //       <div>
+  //       <button className='bg-white rounded-full border-2'>
+  //             <img src={icon1} alt='icon1'/>
+  //           </button>
+  //       </div>
+  //     </div>
+  //   </section>
+  // )
+//}
+
+//export default Cards;
